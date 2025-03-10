8c44d3a34f60efb10573b784ea568f2dfc9cb0f9
Vijay-kumar2006 <vijaykumar.vk3105@gmail.com>
2025-03-10 17:13:07 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import ItemList from "./components/ItemList";
+import { useEffect, useState } from "react";
 
 // use the following link to get the data
 // `/doors` will give you all the doors.
@@ -6,10 +7,24 @@ const API_URI = `https://${import.meta.env.VITE_API_URI}/doors`;
 
 function App() {
   // Get the existing item from the server
-  // const [items, setItems] = useState(null);
+  const [items, setItems] = useState(null);
   // pass the item to UpdateItem as a prop
-
-  return <ItemList />;
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URI);
+        if (!response.ok) {
+          console.log("Failed to fetch data");
+        }
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.log("Error fetching items:", error);
+      }
+    };
+    
+    fetchItems();
+  }, []);
+  return <ItemList key={items.id} items={items} />;
 }
-
 export default App;
